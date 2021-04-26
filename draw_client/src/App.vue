77ca51c3ffd12cdcf253147a13f6dbc4dafd8c02
1400121424 <14001214242QQ.COM>
2021-04-26 23:20:35 +0800

    项目提交

---
+++ draw_client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,body,#app {
+  margin-top: 0%;
+  margin-left: 0%;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ draw_client/src/pages/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+        {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'HelloWorld',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .main{
+        background-color: bisque;
+        height: 100%;
+        width: 100%;
+    }
+</style>
